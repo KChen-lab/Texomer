13d947316e170bc3442233ee9601af479d60a25c
Fang Wang <wfang0828@gmail.com>
2018-11-09 11:52:08 -0600

    Texomer update

--- Texomer.py
+++ Texomer.py
@@ -30,13 +30,15 @@ def VarscanTrans(varscan,case):
         altNumT=line[9]
         gt=line[7]
         type=line[12]
-        if type == "Germline" and gt not in ["A","C","G","T"]:
+        pgerm=float(line[13])
+        psoma=float(line[14])
+        if type == "Germline" and gt not in ["A","C","G","T"] and pgerm < 0.01:
             out=chr+"\t"+position+"\t"+ref+"\t"+var+"\t"+refNumN+"\t"+altNumN+"\t"+refNumT+"\t"+altNumT
             print >> germlineout,out
-        if type == "Somatic":
+        if type == "Somatic" and psoma < 0.01:
             out=chr+"\t"+position+"\t"+ref+"\t"+var+"\t"+refNumN+"\t"+altNumN+"\t"+refNumT+"\t"+altNumT
             print >> somaticout,out
-def SNVexp(germline,somatic,samfile,outputname,case):
+def SNVexp(germline,somatic,samfile,outputname,case,bedtools):
     chro=range(23)
     chro.remove(0)
     chrom=[]
@@ -77,14 +79,15 @@ def SNVexp(germline,somatic,samfile,outputname,case):
     for line in sam:
         line=line[0:-1].split("\t")
         readid=line[0]
-        if "chr" not in line[2]:
-            chr="chr"+line[2]
-        else:
-            chr=line[2]
-        start=line[3]
-        seq=line[9]
-        out=chr+"\t"+start+"\t"+str(int(start)+len(seq)-1)+"\t"+readid+"\t"+seq
-        print >> tempout,out
+        if "@" not in readid:
+            if "chr" not in line[2]:
+                chr="chr"+line[2]
+            else:
+                chr=line[2]
+            start=line[3]
+            seq=line[9]
+            out=chr+"\t"+start+"\t"+str(int(start)+len(seq)-1)+"\t"+readid+"\t"+seq
+            print >> tempout,out
     sam.close()
     tempout.close()
     output=open(outputname,'w')
@@ -101,7 +104,7 @@ def SNVexp(germline,somatic,samfile,outputname,case):
             out=chromosome+"\t"+str(key)+"\t"+str(key)+"\t"+ref+"\t"+alt+"\t"+type
             print >> snvpos,out
     snvpos.close()
-    os.system("intersectBed -a "+case+".bed -b "+case+".snptemp.bed -wa -wb > "+case+".snp.read.bed")
+    os.system(bedtools+" intersect -a "+case+".bed -b "+case+".snptemp.bed -wa -wb > "+case+".snp.read.bed")
     snpdata=open(case+".snp.read.bed")
     read={}
     SNPpos={}
@@ -122,8 +125,7 @@ def SNVexp(germline,somatic,samfile,outputname,case):
             SNPpos.setdefault(chr,[]).append(pos)
             read.setdefault(chr+":"+str(pos),[]).append("ALT\t"+readid)
     snpdata.close()
-    chro=list(set(chrom).intersection(SNPpos.keys()))
-    for chromosome in chro:
+    for chromosome in chrom:
         temppos=sorted(list(set(SNPpos[str(chromosome)])))
         temp=SNV[str(chromosome)]
         for key in temppos:
@@ -149,60 +151,144 @@ def datatrans(input,output):
         print >> outputdata,"chr"+line[0:-1]
     data.close()
     outputdata.close()
+def SNPcalling(Tumor,Normal,samtools,picard,ref,Varscan):
+    os.system(samtools+" index "+Tumor)
+    os.system(samtools+" index "+Normal)
+    tumorpile="tumor.pileup"
+    normalpile="normal.pileup"
+    commond=samtools+" mpileup -f "+ref+" "+Tumor+" > "+tumorpile
+    os.system(commond)
+    commond=samtools+" mpileup -f "+ref+" "+Normal+" > "+normalpile
+    os.system(commond)
+    commond="java -jar "+Varscan+" somatic "+normalpile+" "+tumorpile+" "+"case.varscan"
+    os.system(commond)
+    VarscanTrans(varscan="case.varscan.snp",case="case")
+
+def getPath(path):
+    path1=path.split("/")
+    if path1[0] == ".":
+        if (len(path1)==1):
+            newpath=os.getcwd()
+        else:
+            newpath=os.getcwd()
+            for ele in path1:
+                if ele !=".":
+                    newpath=newpath+"/"+ele
+    elif path1[0]=="..":
+        i = 0
+        for ele in path1:
+            if ele == "..":
+                i=i+1
+        path2=os.getcwd()
+        path2=path2.split("/")
+        newpath="/"+path2[0]
+        if len(path2)-i > 1:
+            for j in range(1,len(path2)-i):
+                newpath=newpath+"/"+path2[j]
+    else:
+        newpath=path
+    return newpath
+
 
 def main():
-    usage = "usage: python %prog <-g germline> <-s somatic> <-r RNA.bam> <-p script path> <-v varscan output>[...]"
-    description = "Input files require RNA-seq data, germline and somatic mutations. We also can convert the output of the somatic programs of Varscan2 through -v if you don't have germline and somatic mutation files. For example: python %prog -g germline -s somatic -R RNA.bam -i RNA.bam.bai -o output. -i and -o are not necessary."
+    usage = "usage: python %prog -p <Texomer path> -I <input form> [-t <tumor bam file>] [-n <normal bam file>] [-r <RNA bam file>] [-v <Variscan mutation calling output>] [-g <Defiend germline mutation input file>] [-s <Defined somatic mutation input file>] [-o <output path>] [-u <Optimazing>] [-e <Defined expression file of mutation>]"
+    description = "The path of Texomer and the form of input file are required in Texomer. Three forms of input file are allowed: BAM, Varscan and Defined. If -I BAM is selected, -t and -n are required. Texomer requires bam file is alligned based on GRCH38. If -I Varscan is selected, -v is required. If -I Defined is selected, -g and -s are required. RNA-seq bam file is optional for Texomer. Texomer would do estimation only at DNA level if without RNA data input."
     op = OptionParser(version="%prog 0.1",description=description,usage=usage,add_help_option=False)
     op.add_option("-h","--help",action="help",
                   help="Show this help message and exit.")
-    op.add_option("-p","--Rscript",dest="Rscript",type="str",
+    op.add_option("-p","--Texomer",dest="Texomer",type="str",
                   help="the path of Texomer")
+    op.add_option("-I","--Input",dest="Input",type="str",
+                  help="the form of input file: BAM, Varscan, and Defined")
+    op.add_option("-t","--Tumor",dest="Tumor",type="str",
+                  help="Tumor WES bam file.")
+    op.add_option("-n","--Normal",dest="Normal",type="str",
+                  help="Normal WES bam file.")
+    op.add_option("-o","--outpath",dest="outpath",type="str",
+                  help="the output path.")
+    op.add_option("-r","--RNA",dest="RNA",type="str",
+                  help="RNA-seq bam file")
     op.add_option("-g","--germline",dest="germline",type="str",
-                  help="You can input your own germline mutation file if no -v. The file name of germline input file include 8 columns: chromosome, position, RefAllele, Altallele, read counts of RefAllele in normal, read counts of Altallele in normal, read counts of RefAllele in tumor and read counts of Altallele in tumor with header seperated by tab. \t")
+                  help="You can input your own germline mutation file together with somatic mutation file (-s). The file include 8 columns: chromosome, position, RefAllele, Altallele, read counts of RefAllele in normal, read counts of Altallele in normal, read counts of RefAllele in tumor and read counts of Altallele in tumor with header seperated by tab. \t")
     op.add_option("-s","--somatic",dest="somatic",type="str",
-                  help="You can input your own somatic mutation file if no -v. The file name of somatic input file include 8 columns: chromosome, position, RefAllele, Altallele, read counts of RefAllele in normal, read counts of Altallele in normal, read counts of RefAllele in tumor and read counts of Altallele in tumor with header seperated by tab. \t")
+                  help="You can input your own somatic mutation file together with germline mutation file (-g). The file include 8 columns: chromosome, position, RefAllele, Altallele, read counts of RefAllele in normal, read counts of Altallele in normal, read counts of RefAllele in tumor and read counts of Altallele in tumor with header seperated by tab. \t")
     op.add_option("-v","--varscan",dest="varscan",type="str",
                   help="the output of Varscan2 based on somatic calling")
-    op.add_option("-r","--RNA",dest="RNA",type="str",
-                  help="bam file of RNA-seq")
-    op.add_option("-i","--bai",dest="bai",type="str",
-                  help="index bam file of RNA-seq. this is optional.")
     op.add_option("-e","--snvexpress",dest="snvexpress",type="str",
                   help="The allelic read count of mutation from RNA-seq including 7 columns: chromosome, position, ref, alt, refnum, altnum and type (germline or somatic)")
-    op.add_option("-t","--iter",dest="iter",type="int",
-                  help="optimal through somatic mutation, 0 corresponding to no optimation and 1 is optimation. The default = 1")
-    op.add_option("-o","--outpath",dest="outpath",type="str",
-                  help="the output path. This is optional")
+    op.add_option("-u","--iter",dest="iter",type="int",
+                  help="optimization using somatic mutation, 0 corresponding to no optimization and 1 is optimization. The default = 1")
     (options,args) = op.parse_args()
-    if not options.Rscript:
+    if not options.Texomer or not options.Input:
         op.print_help()
         sys.exit(1)
+    if options.Input not in ["BAM","Varscan","Defined"]:
+        print "Please select input form from BAM, varscan and Defined!"
+        op.print_help()
+        sys.exit(1)
+    currentPath=os.getcwd()
+    Texomerpath=options.Texomer
+    Texomerpath=getPath(Texomerpath)
+    samtools=Texomerpath+"/samtools"
+    bedtools=Texomerpath+"/bedtools"
+    picard=Texomerpath+"/picard.jar"
+    ref=Texomerpath+"/reference/GRCH38.fa"
+    Varscan=Texomerpath+"/VarScan.v2.3.9.jar"
     if not options.outpath:
         outpath=os.getcwd()
     else:
         outpath=options.outpath
+        outpath=getPath(outpath)
+        outpath1=outpath.split('/')
+        path1="/"
+        for i in range(1,(len(outpath1)-1)):
+            path1=path1+outpath1[i]+"/"
+        if outpath1[len(outpath1)-1] not in os.listdir(path1):
+            os.chdir(path1)
+            os.system("mkdir "+outpath1[len(outpath1)-1])
     os.chdir(outpath)
     os.system("mkdir temp")
-    os.chdir("temp")
-    Rscript = options.Rscript
-    if not options.germline or not options.somatic:
+    os.chdir(currentPath)
+    #Rscript = options.Rscript
+    if options.Input == "Varscan":
         if not options.varscan:
-            op.print_help()
+            print "Please input mutation file matched with Varscan output through -v."
             sys.exit(1)
         else:
             varscan=options.varscan
             print "Transfer varscan2 output"
+            varscan=getPath(varscan)
             case=varscan.split("/")[len(varscan.split("/"))-1]
+            os.chdir(outpath+"/temp")
             VarscanTrans(varscan,case)
             germlinename = os.getcwd()+"/"+case+".germline"
-            if not options.somatic:
-                somaticname = os.getcwd()+"/"+case+".somatic"
-            else:
-                somaticname= options.somatic
-    else:
-        germlinename = options.germline
-        somaticname = options.somatic    
+            somaticname = os.getcwd()+"/"+case+".somatic"
+    elif options.Input == "BAM":
+        if options.Tumor and options.Normal:
+            Tumor=options.Tumor
+            Tumor=getPath(Tumor)
+            Normal=options.Normal
+            Normal=getPath(Normal)
+            case="case"
+            os.chdir(outpath+"/temp")
+            SNPcalling(Tumor=Tumor,Normal=Normal,samtools=samtools,picard=picard,ref=ref,Varscan=Varscan)
+            germlinename = os.getcwd()+"/"+case+".germline"
+            somaticname = os.getcwd()+"/"+case+".somatic"
+            os.system("rm "+Tumor+".bai")
+            os.system("rm "+Normal+".bai")
+        else:
+            print "Please input bam file through -t and -n."
+            sys.exit(1)
+    elif options.Input == "Defined":
+        if options.germline and options.somatic:
+            germlinename = options.germline
+            germlinename=getPath(germlinename)
+            somaticname = options.somatic
+            somaticname=getPath(somaticname)
+        else:
+            print "Please input defined germline and somatic mutation files through -g and -s."
+            sys.exit(1)
+    os.chdir(outpath+"/temp")
     data=open(somaticname)
     next(data)
     line=next(data)
@@ -221,45 +307,46 @@ def main():
         germlineoutname = os.getcwd()+"/"+case+".germline"
         datatrans(input=germlinename,output=germlineoutname)
         germlinename=germlineoutname
-    if not options.snvexpress:
-        if not options.RNA:
+    os.chdir(currentPath)
+    if not options.RNA:
+        if not options.snvexpress:
             print "No RNA data! Only estimate at DNA level!"
-            outputname="NA"
-            #op.print_help()
-            #sys.exit(1)
+            RNAname="NA"
         else:
-            RNAname = options.RNA
-            case=RNAname.split("/")[len(RNAname.split("/"))-1]
-            if RNAname.split(".")[len(RNAname.split("."))-1] == "bam":
-                if not options.bai:
-                    print "Generate index"
-                    os.system("samtools index "+RNAname)
-                print "Transfer bam to sam"
-                os.system("samtools view "+RNAname+" > "+RNAname+".sam")
-                samfilename=RNAname+".sam"
-            else:
-                samfilename=RNAname
-            outputname=case+".SNV"
-            if os.path.exists(outputname):
-                os.remove(outputname)
-            print "Align mutations to RNA data"
-            SNVexp(germline=germlinename,somatic=somaticname,samfile=samfilename,outputname=outputname,case=case)
+            RNAname=options.snvexpress
+            RNAname=getPath(RNAname)
+            data=open(RNAname)
+            next(data)
+            line=next(data)[0:-1].split("\t")
+            data.close()
+            if "chr" not in line[0]:
+                case=outputname.split("/")[len(outputname.split("/"))-1]
+                os.chdir(outpath+"/temp")
+                datatrans(input=outputname,output=case)
+                #outputname=case
     else:
-        outputname=options.snvexpress
-        data=open(outputname)
-        next(data)
-        line=next(data)[0:-1].split("\t")
-        data.close()
-        if "chr" not in line[0]:
-            case=outputname.split("/")[len(outputname.split("/"))-1]
-            datatrans(input=outputname,output=case)
-            outputname=case
+        RNAname = options.RNA
+        RNAname=getPath(RNAname)
+        case=RNAname.split("/")[len(RNAname.split("/"))-1]
+        print "Transfer bam to sam"
+        os.chdir(outpath+"/temp")
+        os.system(samtools+ " index "+RNAname)
+        os.system(samtools+" view "+RNAname+" > "+RNAname+".sam")
+        samfilename=RNAname+".sam"
+        outputname=case+".SNV"
+        if os.path.exists(outputname):
+            os.remove(outputname)
+        print "Align mutations to RNA data"
+        SNVexp(germline=germlinename,somatic=somaticname,samfile=samfilename,outputname=outputname,case=case,bedtools=bedtools)
+        os.system("rm "+RNAname+".sam")
+        os.system("rm "+RNAname+".bai")
+        RNAname=outputname
     print "Infering at DNA level"
     if not options.iter:
         optindex=1
     else:
         optindex=options.iter
-    os.system("Rscript "+Rscript+"/Texomer.R "+germlinename+" "+somaticname+" "+outputname+" "+Rscript+" "+outpath+" "+str(optindex))
+    os.system("Rscript "+Texomerpath+"/Texomer.R "+germlinename+" "+somaticname+" "+RNAname+" "+Texomerpath+" "+outpath+" "+str(optindex))
     os.chdir(outpath)
     os.system("rm -r temp")
 
